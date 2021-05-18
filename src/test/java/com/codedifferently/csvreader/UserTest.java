f8f9ebef7224bde60cf0a6ec3f79f85c7d3a608b
Matt Werth <matthewwerth@gmail.com>
2021-05-18 09:58:18 -0400

    Added User Tests

---
+++ src/test/java/com/codedifferently/csvreader/UserTest.java
@@ -0,0 +1,33 @@
+package com.codedifferently.csvreader;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach // create new User object
+    void setUp() {
+    }
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void getCountryCode() {
+    }
+
+    @Test
+    void getAge() {
+    }
+}
